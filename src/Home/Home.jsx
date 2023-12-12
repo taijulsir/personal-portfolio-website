e54a1b25c4d2552fafce8876224c619cc6dbd6ba
taijulsir <princetaijul123@gmail.com>
2023-12-12 11:28:23 +0600

    added tooling in homepage

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,11 +1,13 @@
 
 import Navbar from "../Navbar/Navbar"
+import Tooling from "../Tooling/Tolling";
 import Banner from "./Banner";
 const Home = () => {
     return (
         <div className=" container mx-auto">
           <Navbar></Navbar>
            <Banner id="home"></Banner>
+           <Tooling></Tooling>
         </div>
     );
 };
